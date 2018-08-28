70a074f5fd7a8f5026b567326b219cbc6e41e496
Chris <chris@cypheros.co>
2018-08-28 09:58:18 -0400

    Settings: Move IMEI information to SIM status
    This information is sensitive and should not be in the forefront of the about phone menu.
    
    - Also removed phone number preference as this is already in sim status
    
    Co-authored-by: 33bca <fabian.leutenegger@bluewin.ch>
    Change-Id: I5d6fa97cd59f0f2f394d95497950afff3018bf69
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -39,7 +39,6 @@ import com.android.settings.deviceinfo.RegulatoryInfoPreferenceController;
 import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
 import com.android.settings.deviceinfo.UptimePreferenceController;
 import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
-import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.EntityHeaderController;
