pixeldust-project-caf/android_packages_apps_Settings

Import AppLockSettings [SQUASHED]. * from https://github.com/Flamingo-OS/packages_apps_FlamingoSettings
[spezi77]
* Use our custom TogglePreferenceController and abstract DashboardFragment

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/pixeldust/settings/security/applock/AppLockBiometricPreferenceController.kt
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixeldust.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.pixeldust.settings.preference.PixeldustTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "app_lock_biometrics_allowed"
+
+class AppLockBiometricPreferenceController(
+    context: Context,
+    private val coroutineScope: CoroutineScope
+) : PixeldustTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private val biometricManager = context.getSystemService(BiometricManager::class.java)
+
+    private var preference: Preference? = null
+    private var isBiometricsAllowed = false
+
+    init {
+        coroutineScope.launch {
+            isBiometricsAllowed = withContext(Dispatchers.Default) {
+                appLockManager.isBiometricsAllowed()
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus(): Int {
+        val result = biometricManager.canAuthenticate(BIOMETRIC_STRONG)
+        return if (result == BiometricManager.BIOMETRIC_SUCCESS) AVAILABLE else CONDITIONALLY_UNAVAILABLE
+    }
+
+    override fun isChecked() = isBiometricsAllowed
+
+    override fun setChecked(checked: Boolean): Boolean {
+        if (isBiometricsAllowed == checked) return false
+        isBiometricsAllowed = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setBiometricsAllowed(isBiometricsAllowed)
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/pixeldust/settings/security/applock/AppLockHideAppPC.kt
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixeldust.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.pixeldust.settings.preference.PixeldustTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "hide_from_launcher"
+
+class AppLockHideAppPC(
+    context: Context,
+    private val packageName: String,
+    private val coroutineScope: CoroutineScope
+) : PixeldustTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private var hideFromLauncher = AppLockManager.DEFAULT_HIDE_IN_LAUNCHER
+    private var preference: Preference? = null
+
+    init {
+        coroutineScope.launch {
+            hideFromLauncher = withContext(Dispatchers.Default) {
+                appLockManager.hiddenPackages.any { it == packageName }
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun isChecked() = hideFromLauncher
+
+    override fun setChecked(checked: Boolean): Boolean {
+        if (hideFromLauncher == checked) return false
+        hideFromLauncher = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setPackageHidden(packageName, hideFromLauncher)
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/pixeldust/settings/security/applock/AppLockNotificationRedactionPC.kt
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixeldust.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.pixeldust.settings.preference.PixeldustTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "redact_notifications"
+
+class AppLockNotificationRedactionPC(
+    context: Context,
+    private val packageName: String,
+    private val coroutineScope: CoroutineScope
+) : PixeldustTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private var shouldRedactNotification = AppLockManager.DEFAULT_REDACT_NOTIFICATION
+    private var preference: Preference? = null
+
+    init {
+        coroutineScope.launch {
+            shouldRedactNotification = withContext(Dispatchers.Default) {
+                appLockManager.packageData.find {
+                    it.packageName == packageName
+                }?.shouldRedactNotification == true
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun isChecked() = shouldRedactNotification
+
+    override fun setChecked(checked: Boolean): Boolean {
+        if (shouldRedactNotification == checked) return false
+        shouldRedactNotification = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setShouldRedactNotification(packageName, checked)
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/pixeldust/settings/security/applock/AppLockPackageProtectionPC.kt
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixeldust.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.pixeldust.settings.preference.PixeldustTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "main_switch"
+
+class AppLockPackageProtectionPC(
+    context: Context,
+    private val packageName: String,
+    private val coroutineScope: CoroutineScope
+) : PixeldustTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private var isProtected = false
+    private var preference: Preference? = null
+
+    init {
+        coroutineScope.launch {
+            isProtected = withContext(Dispatchers.Default) {
+                appLockManager.packageData.any {
+                    it.packageName == packageName
+                }
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun isChecked() = isProtected
+
+    override fun setChecked(checked: Boolean): Boolean {
+        if (isProtected == checked) return false
+        isProtected = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            if (isProtected) {
+                appLockManager.addPackage(packageName)
+            } else {
+                appLockManager.removePackage(packageName)
+            }
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/pixeldust/settings/security/applock/AppLockSettingsFragment.kt
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixeldust.settings.security.applock
+
+import android.content.Context
+import android.os.SystemProperties
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+import androidx.lifecycle.lifecycleScope
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.search.SearchIndexable
+
+import com.pixeldust.settings.preference.PixeldustDashboardFragment
+
+@SearchIndexable
+class AppLockSettingsFragment : PixeldustDashboardFragment(),
+    MenuItem.OnMenuItemClickListener {
+
+    private var debugEnabled = SystemProperties.get(DEBUG_PROPERTY, null) == LEVEL_DEBUG
+    private var handledClick = false
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_settings
+
+    override protected fun getLogTag() = TAG
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater)
+        menu.add(
+            0 /* groupId */,
+            MENU_ITEM_DEBUG_ID,
+            0 /* order */,
+            getDebugMenuItemTitle(),
+        ).setOnMenuItemClickListener(this)
+    }
+
+    private fun getDebugMenuItemTitle(): Int =
+        if (debugEnabled) R.string.disable_debugging else R.string.enable_debugging
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        if (item.itemId == MENU_ITEM_DEBUG_ID) {
+            debugEnabled = !debugEnabled
+            SystemProperties.set(DEBUG_PROPERTY, if (debugEnabled) LEVEL_DEBUG else null)
+            item.setTitle(getDebugMenuItemTitle())
+            return true
+        }
+        return false
+    }
+
+    override protected fun createPreferenceControllers(
+        context: Context
+    ) : List<AbstractPreferenceController> = listOf(
+        AppLockBiometricPreferenceController(context, lifecycleScope)
+    )
+
+    companion object {
+        private const val TAG = "AppLockSettingsFragment"
+
+        private const val DEBUG_PROPERTY = "log.tag.AppLockManagerService"
+        private const val LEVEL_DEBUG = "DEBUG"
+        private const val MENU_ITEM_DEBUG_ID = 101
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.app_lock_settings)
+    }
+}

rdx420/packages_apps_Settings

src/com/android/settings/security/applock/AppLockBiometricPreferenceController.kt
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.android.settings.preference.BlazeTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "app_lock_biometrics_allowed"
+
+class AppLockBiometricPreferenceController(
+    context: Context,
+    private val coroutineScope: CoroutineScope
+) : BlazeTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private val biometricManager = context.getSystemService(BiometricManager::class.java)
+
+    private var preference: Preference? = null
+    private var isBiometricsAllowed = false
+
+    init {
+        coroutineScope.launch {
+            isBiometricsAllowed = withContext(Dispatchers.Default) {
+                appLockManager.isBiometricsAllowed()
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus(): Int {
+        val result = biometricManager.canAuthenticate(BIOMETRIC_STRONG)
+        return if (result == BiometricManager.BIOMETRIC_SUCCESS) AVAILABLE else CONDITIONALLY_UNAVAILABLE
+    }
+
+    override fun isChecked() = isBiometricsAllowed
+
+    override fun setChecked(checked: Boolean): Boolean {
+        if (isBiometricsAllowed == checked) return false
+        isBiometricsAllowed = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setBiometricsAllowed(isBiometricsAllowed)
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/android/settings/security/applock/AppLockHideAppPC.kt
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.android.settings.preference.BlazeTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "hide_from_launcher"
+
+class AppLockHideAppPC(
+    context: Context,
+    private val packageName: String,
+    private val coroutineScope: CoroutineScope
+) : BlazeTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private var hideFromLauncher = AppLockManager.DEFAULT_HIDE_IN_LAUNCHER
+    private var preference: Preference? = null
+
+    init {
+        coroutineScope.launch {
+            hideFromLauncher = withContext(Dispatchers.Default) {
+                appLockManager.hiddenPackages.any { it == packageName }
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun isChecked() = hideFromLauncher
+
+    override fun setChecked(checked: Boolean): Boolean {
+        if (hideFromLauncher == checked) return false
+        hideFromLauncher = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setPackageHidden(packageName, hideFromLauncher)
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/android/settings/security/applock/AppLockNotificationRedactionPC.kt
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.android.settings.preference.BlazeTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "redact_notifications"
+
+class AppLockNotificationRedactionPC(
+    context: Context,
+    private val packageName: String,
+    private val coroutineScope: CoroutineScope
+) : BlazeTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private var shouldRedactNotification = AppLockManager.DEFAULT_REDACT_NOTIFICATION
+    private var preference: Preference? = null
+
+    init {
+        coroutineScope.launch {
+            shouldRedactNotification = withContext(Dispatchers.Default) {
+                appLockManager.packageData.find {
+                    it.packageName == packageName
+                }?.shouldRedactNotification == true
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun isChecked() = shouldRedactNotification
+
+    override fun setChecked(checked: Boolean): Boolean {
+        if (shouldRedactNotification == checked) return false
+        shouldRedactNotification = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setShouldRedactNotification(packageName, checked)
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/android/settings/security/applock/AppLockPackageProtectionPC.kt
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.android.settings.preference.BlazeTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "main_switch"
+
+class AppLockPackageProtectionPC(
+    context: Context,
+    private val packageName: String,
+    private val coroutineScope: CoroutineScope
+) : BlazeTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private var isProtected = false
+    private var preference: Preference? = null
+
+    init {
+        coroutineScope.launch {
+            isProtected = withContext(Dispatchers.Default) {
+                appLockManager.packageData.any {
+                    it.packageName == packageName
+                }
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun isChecked() = isProtected
+
+    override fun setChecked(checked: Boolean): Boolean {
+        if (isProtected == checked) return false
+        isProtected = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            if (isProtected) {
+                appLockManager.addPackage(packageName)
+            } else {
+                appLockManager.removePackage(packageName)
+            }
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/android/settings/security/applock/AppLockSettingsFragment.kt
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.content.Context
+import android.os.SystemProperties
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+import androidx.lifecycle.lifecycleScope
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.search.SearchIndexable
+
+import com.android.settings.preference.BlazeDashboardFragment
+
+@SearchIndexable
+class AppLockSettingsFragment : BlazeDashboardFragment(),
+    MenuItem.OnMenuItemClickListener {
+
+    private var debugEnabled = SystemProperties.get(DEBUG_PROPERTY, null) == LEVEL_DEBUG
+    private var handledClick = false
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_settings
+
+    override protected fun getLogTag() = TAG
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater)
+        menu.add(
+            0 /* groupId */,
+            MENU_ITEM_DEBUG_ID,
+            0 /* order */,
+            getDebugMenuItemTitle(),
+        ).setOnMenuItemClickListener(this)
+    }
+
+    private fun getDebugMenuItemTitle(): Int =
+        if (debugEnabled) R.string.disable_debugging else R.string.enable_debugging
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        if (item.itemId == MENU_ITEM_DEBUG_ID) {
+            debugEnabled = !debugEnabled
+            SystemProperties.set(DEBUG_PROPERTY, if (debugEnabled) LEVEL_DEBUG else null)
+            item.setTitle(getDebugMenuItemTitle())
+            return true
+        }
+        return false
+    }
+
+    override protected fun createPreferenceControllers(
+        context: Context
+    ) : List<AbstractPreferenceController> = listOf(
+        AppLockBiometricPreferenceController(context, lifecycleScope)
+    )
+
+    companion object {
+        private const val TAG = "AppLockSettingsFragment"
+
+        private const val DEBUG_PROPERTY = "log.tag.AppLockManagerService"
+        private const val LEVEL_DEBUG = "DEBUG"
+        private const val MENU_ITEM_DEBUG_ID = 101
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.app_lock_settings)
+    }
+}